fadde34a5b9e196e224a6bf7f1a5e3626df5f400
P451N0 <96614460+P451N0@users.noreply.github.com>
2022-06-08 10:13:48 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -2,7 +2,7 @@
 
 $useragent = "Mozilla/5.0 (Linux; Android 10; Infinix X692) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Mobile Safari/537.36";
 
-$url_claim_fp = "https://uptocoin.my.id/fp/faucet.php?address=TBi1yHU6n1jYQH98wJELxE2C5ughURvoGw&currency=DOGE&key=bc0ded00aefee35a64083bf49ce54a78";
+$url_claim_fp = "https://uptocoin.my.id/fp/faucet.php?r=TBi1yHU6n1jYQH98wJELxE2C5ughURvoGw&rc=DOGE&address=DE1CRbWktM5vZgDtMZUjkDTh17VLk4mGeV&currency=DOGE&key=29fb3a91e1375bc1e30463be926d4026";
 
 $cookie_fp = "a=wG1wftE31xUltWX1nRL3Bc215tlpmvr3; _popfired_expires=Invalid%20Date; token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYpdOkwFil07sgAGBAsAAIIOoVatVFpnShXjC-uz1NlL3IbigoPBWK2EcrH9YGio0wQBGMEQCIAt_eErRB0nQ5smD1-JD_PpIPDEApFHhtzO26A6f-UlEAiALQaK9Y1qkblwzN7GJyXrqT4w92apkEUS48gaJyafq7w; _popfired=4; lastOpenAt_=1654079794403";
 
