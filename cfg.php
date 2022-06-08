7e39e349bc5875c7a9b34d27d92069880942b297
P451N0 <96614460+P451N0@users.noreply.github.com>
2022-06-08 10:08:41 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,10 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 10; Infinix X692) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Mobile Safari/537.36";
+
+$url_claim_fp = "https://uptocoin.my.id/fp/faucet.php?address=TBi1yHU6n1jYQH98wJELxE2C5ughURvoGw&currency=DOGE&key=bc0ded00aefee35a64083bf49ce54a78";
+
+$cookie_fp = "a=wG1wftE31xUltWX1nRL3Bc215tlpmvr3; _popfired_expires=Invalid%20Date; token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYpdOkwFil07sgAGBAsAAIIOoVatVFpnShXjC-uz1NlL3IbigoPBWK2EcrH9YGio0wQBGMEQCIAt_eErRB0nQ5smD1-JD_PpIPDEApFHhtzO26A6f-UlEAiALQaK9Y1qkblwzN7GJyXrqT4w92apkEUS48gaJyafq7w; _popfired=4; lastOpenAt_=1654079794403";
+
+
+
